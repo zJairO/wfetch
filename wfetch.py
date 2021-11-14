9cb08f8968976968aa28a4c9a0f9c6a14d9bd3f9
zJairO <zjairo@outlook.com>
2021-11-14 12:23:11 -0600

    Update

--- wfetch.py
+++ wfetch.py
@@ -1,24 +1,5 @@
 import os, socket, platform, subprocess, sys, ctypes
-
-class Computer:
-    def __init__(self, user, name, osn, host, version, uptime):
-        self.user = user
-        self.name = name
-        self.osn = osn
-        self.host = host
-        self.version = version
-        self.uptime = uptime
-
-    def ascii(self):
-        return f"""
-########@######## {self.user}@{self.name}
-########@######## host    {self.host}
-########@######## os      {self.osn}
-@@@@@@@@@@@@@@@@@ version {self.version}
-########@######## uptime  {self.uptime}
-########@########
-########@######## 
-"""
+from settings import Computer
 
 def getHost():
     command = "wmic baseboard get product"
@@ -34,8 +15,6 @@ def getUpTime():
     hour, mins = divmod(mins, 60)
 
     return f"{hour:01}h {mins:01}m"
-
-    
 
 win = Computer(os.getlogin(), socket.gethostname(), platform.system() + " " + platform.release(), getHost(), sys.getwindowsversion().build, getUpTime())
 
